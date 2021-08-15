653e47cffac1cfb07a30e2449a0be18518b9f4ee
Mustafa Toker <mustafa.toker@kboxglobal.comy>
2021-08-16 01:48:21 +0300

    undo test

--- routes/router.go
+++ routes/router.go
@@ -7,7 +7,7 @@ import (
 
 func SetupRoutes(r *gin.Engine, c *controllers.TodoController) {
 	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{"message": "pong!"})
+		c.JSON(200, gin.H{"message": "pong"})
 	})
 	r.GET("/api/v1/todos", c.Index)
 	r.POST("/api/v1/todos", c.Create)
